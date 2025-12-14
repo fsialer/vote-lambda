8e4ca2fa660ef255ccb3f6b5692e75f17cb59cf7
Fernando <asialer05@hotmail.com>
2025-12-14 12:38:58 -0500

    refactor: improve connection of redis in PoolGetFunction

--- PoolGetFunction/src/main/java/com/fernando/vote/poolget/config/RedisConfig.java
+++ PoolGetFunction/src/main/java/com/fernando/vote/poolget/config/RedisConfig.java
@@ -7,13 +7,15 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisConfig {
     private static JedisPool jedisPool;
 
-    static{
-        String host= System.getenv("REDIS_HOST");
-        Integer port= Integer.valueOf(System.getenv("REDIS_PORT"));
-        String password= System.getenv("REDIS_PASSWORD");
-        JedisPoolConfig poolConfig=new JedisPoolConfig();
-        poolConfig.setMaxTotal(128);
-        poolConfig.setMaxIdle(128);
+    private static void init() {
+        String host = System.getenv("REDIS_HOST");
+        int port = Integer.parseInt(System.getenv("REDIS_PORT"));
+        String password = System.getenv("REDIS_PASSWORD");
+
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
 
         if (password == null || password.isBlank()) {
             jedisPool = new JedisPool(poolConfig, host, port, 5000);
@@ -23,6 +25,9 @@ public class RedisConfig {
     }
 
     public static Jedis getResource(){
+        if (jedisPool == null) {
+            init();
+        }
         return jedisPool.getResource();
     }
 }
